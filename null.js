cabfca4515bb7142b4661e2933e032d63dd52d63
sameerareddy25 <ravulapatisameera@klh.edu.in>
2026-05-01 11:37:46 +0530

    Add files via upload

---
+++ null.js
@@ -0,0 +1,8 @@
+let name = null;
+let username = " ";
+let marks = 0;
+let city;
+console.log("Name:", name ?? "Guest");
+console.log("Username:", username ?? "Guest");
+console.log("Marks:", marks ?? 50);
+console.log("City:", city ?? "Hyderabad");
