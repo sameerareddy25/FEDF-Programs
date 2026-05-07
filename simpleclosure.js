56e199f1b1c46f9beb0bcf3f4514d0bb0280c1d8
sameerareddy25 <ravulapatisameera@klh.edu.in>
2026-05-07 08:36:14 +0530

    Add files via upload

---
+++ simpleclosure.js
@@ -0,0 +1,14 @@
+function outer() {
+  let count = 0;
+
+  return function inner() {
+    count++;
+    return count;
+  };
+}
+
+const counter = outer();
+
+console.log(counter()); // 1
+console.log(counter()); // 2
+console.log(counter()); // 3
